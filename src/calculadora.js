79b8d77defaf05a44ca1182663b7f0053886b78d
VictorSerafim <victorfrancoserafim01@gmail.com>
2022-09-15 23:46:58 -0300

    ajuste funcionalidades,servicos e tests

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './calculadora.css';
 import {Jumbotron, Container ,Row, Col, Button,Form} from 'react-bootstrap';
+import CalculadoraService from "./calculadora.service";
 
 function Calculadora() {
+
+  const [calcular, concaternarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO]=CalculadoraService();
+
+  const [txtNumeros,setTxtNumeros] = useState('0');
+  const [numero1,setNumero1] = useState('0');
+  const [numero2,setNumero2] = useState(null);
+  const [operacao,setOperacao] = useState(null);
+
+  function adicionarNumero(numero){
+    let resultado;
+    if(operacao===null){
+      resultado=concaternarNumero(numero1, numero);
+      setNumero1(resultado);
+    }else{
+      resultado=concaternarNumero(numero2, numero);
+      setNumero2(resultado);
+    }
+    setTxtNumeros(resultado);
+  }
+  function definirOperacao(op){
+    //define a operacao caso nao exista
+     if(operacao===null){
+      setOperacao(op);
+      return;
+     }
+    //caso a operacao estiver definida e numero 2 seleciona, realiza calculo 
+    if(numero2 !== null){
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+      setOperacao(op);
+      setNumero1(resultado.toString());
+      setNumero2(null);
+      setTxtNumeros(resultado.toString());
+    }
+
+  }
+
+  function acaoCalcular(){
+    if(numero2===null){
+      return;
+    }
+    const resultado =calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+    setTxtNumeros(resultado.toString());
+  }
+
+  function limpar(){
+    setTxtNumeros('0');
+    setNumero1('0');
+    setNumero2(null);
+    setOperacao(null);
+  }
+
   return (
     <Jumbotron style={{
       background:'transparent !important',
@@ -15,35 +67,35 @@ function Calculadora() {
     <Container>
       <Row>
         <Col xs="3">
-          <Button variant='danger'>C</Button>
+          <Button variant='danger'  onClick={limpar}>C</Button>
         </Col>
         <Col xs="9">
-          <Form.Control type='text' name='txtNumeros' class='text-right' readOnly='readOnly'/>
+          <Form.Control type='text' name='txtNumeros' className='text-right' readOnly='readOnly' value={txtNumeros} data-testid="txtNumeros" />
         </Col>
       </Row>
       <Row>
-        <Col><Button variant='light'>7</Button></Col>
-        <Col><Button variant='light'>8</Button></Col>
-        <Col><Button variant='light'>9</Button></Col>
-        <Col><Button variant='warning'>/</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('7')}>7</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('8')}>8</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('9')}>9</Button></Col>
+        <Col><Button variant='warning' onClick={()=>definirOperacao(DIVISAO)}>/</Button></Col>
       </Row>
       <Row>
-        <Col><Button variant='light'>4</Button></Col>
-        <Col><Button variant='light'>5</Button></Col>
-        <Col><Button variant='light'>6</Button></Col>
-        <Col><Button variant='warning'>*</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('4')}>4</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('5')}>5</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('6')}>6</Button></Col>
+        <Col><Button variant='warning' onClick={()=>definirOperacao(MULTIPLICACAO)}>*</Button></Col>
       </Row>
       <Row>
-        <Col><Button variant='light'>1</Button></Col>
-        <Col><Button variant='light'>2</Button></Col>
-        <Col><Button variant='light'>3</Button></Col>
-        <Col><Button variant='warning'>-</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('1')}>1</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('2')}>2</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('3')}>3</Button></Col>
+        <Col><Button variant='warning' onClick={()=>definirOperacao(SUBTRACAO)}>-</Button></Col>
       </Row>
       <Row>
-        <Col><Button variant='light'>0</Button></Col>
-        <Col><Button variant='light'>.</Button></Col>
-        <Col><Button variant='success'>=</Button></Col>
-        <Col><Button variant='warning'>+</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('0')}>0</Button></Col>
+        <Col><Button variant='light' onClick={()=>adicionarNumero('.')}>.</Button></Col>
+        <Col><Button variant='success' onClick={acaoCalcular} >=</Button></Col>
+        <Col><Button variant='warning' onClick={()=>definirOperacao(SOMA)}>+</Button></Col>
       </Row>
     </Container>
             
